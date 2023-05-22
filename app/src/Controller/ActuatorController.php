235aad9ec607192eb9991be009e7e0f124b7b62f
Witold <47141012+JaWitold@users.noreply.github.com>
2023-05-22 21:40:37 +0200

    [SPT-3] dodanie swaggera (#5)

--- app/src/Controller/ActuatorController.php
+++ app/src/Controller/ActuatorController.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Health;
 use App\Service\HealthService;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Attributes as OA;
 
 class ActuatorController extends AbstractController
 {
-    #[Route('/health', name: 'health')]
+    #[Route('/api/health', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful response',
+        content: new Model(type: Health::class),
+    )]
+    #[OA\Tag(name: 'health')]
     public function health(HealthService $healthService): JsonResponse
     {
         return new JsonResponse($healthService());
--- app/src/Model/Health.php
+++ app/src/Model/Health.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace App\Model;
 
 use App\Enum\HealthStatus;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Uid\Uuid;
 
+#[OA\Schema(title: 'Health', description: 'App health status')]
 readonly class Health
 {
+    #[OA\Property(type: 'string', format: 'uuid')]
     public Uuid $flowId;
 
-    public function __construct(public HealthStatus $status = HealthStatus::OK)
+    public function __construct(#[OA\Property] public HealthStatus $status = HealthStatus::OK)
     {
         $this->flowId = Uuid::v4();
     }
